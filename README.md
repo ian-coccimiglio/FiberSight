9ee5133df6a21cccd2debf0b33e1d16c484d38a3
Ian Coccimiglio <icoccimi@gmail.com>
2024-07-25 17:00:30 -0700

    Changed directory layout

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This plugin is meant to do 4 things:
 4) Skeletal Muscle Fibertyping
 
 # Usage #
-The central design of FiberSight requires that for each analysis, you provide the following folder structure.  The sampleIDs should be a unique combination of letters/numbers, and separated from the rest of the string using underscores. 
+The central design of FiberSight requires that for each analysis, you provide the following folder structure.  The sampleID should be unique for each sample. 
 
 (Example)
 - Experiment\_folder
@@ -19,4 +19,25 @@ The central design of FiberSight requires that for each analysis, you provide th
 
 Notes:
 - The Experiment folder can have any name.
-- All raw files should be placed in a subfolder called 'raw'.
+- All raw files should be placed in a subfolder called 'raw'
+
+# Design #
+FiberSight is split into 3 layers of functionality:
+FiberSight Experiment Layer -> Batch Layer -> Single-Image Layer.
+
+The Experiment layer: Performs a guided end-to-end analysis.
+- Designed for streamlined usage
+- Calls lower-level scripts in a fixed order
+- Generates and saves spreadsheets, figures, masks, and analyses
+- Limited flexibility/modifiability.
+
+The Batch Layer: Performs intermediate processing on a batch of images/ROIs
+- Designed to expedite running a partial analysis procedure on all raw images.
+- Processing parameters fixed across images. 
+
+The Single-Image Layer: Processes a single image/ROI.
+- Designed to process one image or ROI at a time
+- Useful for optimizing models/parameters
+
+This layered approach means that you can use individual macro tools of FiberSight (such as edge exclusion, and Cellpose autoprocessing), or a full workflow (to run an entire muscle-imaging workflow).
+
---
+++ src/main/resources/scripts/FiberSight_Beta/Interactive_Tools/Exclude_Border_Interactive_2.py
@@ -0,0 +1,78 @@
+# #@ File (label="Select a label image", style="file") label_image_path
+#@ Boolean (label="Separate labels?", description="Helps with edge masking if borders are heavily in contact with one another", value=True) sep_label
+#@ Boolean (label="Use your GPU?", description="Enables precise and fast masking of labels", value=True) gpu
+from ij import IJ, Prefs, WindowManager as WM
+import os
+from ij.gui import WaitForUserDialog
+from image_tools import runCellpose, pickImage
+from jy_tools import closeAll, reload_modules
+from ij.plugin.frame import RoiManager
+from remove_edge_labels import make_excluded_edges, separate_labels_on_gpu
+
+Prefs.blackBackground = False
+
+if "Labels_Excluded_Edge" in WM.getImageTitles():
+	IJ.selectWindow("Labels_Excluded_Edge")
+	IJ.getImage().close()
+
+IJ.run("Remove Overlay", "");
+RM = RoiManager()
+rm = RM.getRoiManager()
+rm.runCommand("Show All without labels")
+IJ.setTool("polygon")
+imp = IJ.getImage()
+IJ.selectWindow(imp.title)
+roiWait = WaitForUserDialog("Draw", "Draw a closed selection or freehand ROI, then hit OK")
+roiWait.show()
+r2l_prefix="ROIs2Label_"
+curr_selection = imp.getRoi()
+
+if sep_label == True:
+	if gpu and any([plugin.startswith("clij2_") for plugin in os.listdir(IJ.getDirectory("plugins"))]):
+		IJ.run("ROIs to Label image", "")
+		label_image_title = r2l_prefix+imp.title
+		label_image = pickImage(label_image_title)
+		separated_labels = separate_labels_on_gpu(label_image)
+		separated_labels.hide()
+		label_image.hide()
+		edgeless = make_excluded_edges(separated_labels, border_roi=curr_selection)
+	else:
+		IJ.log("CLIJ2 plugin not found!")
+		IJ.log("### Separating Labels by ROI Erosion ###")
+		rm_small = shrink_rois(rm_fibers, imp)
+		IJ.log("### Converting ROIs to Label image ###")
+		IJ.run("ROIs to Label image", "")
+		separated_label_image_title = r2l_prefix+imp.title
+		separated_label_image = pickImage(separated_label_image_title)
+		separated_label_image.hide()
+		IJ.log("### Running Excluded Edge ###")
+		edgeless = make_excluded_edges(separated_label_image, border_roi=border_roi)
+else:
+	IJ.log("### Converting ROIs to Label image ###")
+	IJ.run("ROIs to Label image", "")
+	label_image = IJ.getImage()
+	IJ.log("### Running Excluded Edge ###")
+	edgeless = make_excluded_edges(label_image, border_roi=border_roi)
+
+edgeless.setRoi(curr_selection, True)
+imp.setRoi(curr_selection, True)
+
+IJ.run(imp, "Add Image...", "image=Labels_Excluded_Edge x=0 y=0 opacity=50");
+IJ.run(imp, "Add Selection...", "")
+edgeless.show()
+# IJ.run(edgeless, "Add Selection...", "")
+
+IJ.log("Done!")
+
+
+#
+#if rm.getCount() == 0:
+#	roi = labels.getRoi()
+#	rm.addRoi(roi)
+#
+#if sep_label == True:
+#	if gpu == False:
+#		pass # TODO: Fix this.
+#	else: 
+#		labels = separate_labels_on_gpu(labels)
+#edgeless = make_excluded_edges(labels, rm=rm)
