480ec9ca558a1190fde0d79175b0259a0e09018a
ian-coccimiglio <icoccimi@gmail.com>
2025-01-10 15:24:53 -0800

    Updated script files

--- src/main/resources/scripts/FiberSight_Beta/Batch_Imaging_Tools/Cellpose_Batch_Processor.py
+++ src/main/resources/scripts/FiberSight_Beta/Batch_Imaging_Tools/Cellpose_Batch_Processor.py
@@ -4,17 +4,14 @@
 #@ String(visibility=MESSAGE, value="<html>Filter for a specific string in the file name</html>") docmsg
 #@ String(label='Filter', value='') filters
 #@ String(visibility=MESSAGE, value="<html>Set 0 if the images are single-channel</html>") docChannel
-#@ Integer (label="Border Channel", min=0, max=10, value=4) segChan
-#@ Integer (label="Object Diameter", min=0, max=200, value=0) cellposeDiam
-#@ String (choices={"cyto3", "cyto2", "PSR", "WGA", "HE"}, description="The type of model to use", value="cyto3", style="radioButtonHorizontal") model
+#@ Integer (label="Segmentation Channel", min=0, max=10, value=4, description="<html>Set 0 to use gray-scale, otherwise channels are indexed from 1</html>",) segChan
+#@ Integer (label="Object Diameter", min=0, max=200, value=0, description="<html>Set 0 to use Cellpose auto-detection (available only on cyto3 model) </html>") cellposeDiam
+#@ String (choices={"cyto3", "PSR_9", "WGA_21", "HE_30"}, description="The type of model to use", value="cyto3", style="radioButtonHorizontal") model
 
 ''' Cellpose Autoprocessor. 
 1) Loads in a directory
-2) Creates a directory for (flattened/processed) images
-3) Creates a directory for ROIs
-4) Creates a results file at the folder level
-5) Processes all images using sensible defaults for cellpose
-
+2) Creates a directory for ROIs if one doesn't exist
+3) Processes all images using sensible defaults for cellpose
 '''
 
 import os
@@ -30,31 +27,22 @@ IJ.log("### Running Cellpose Batch mode ###")
 def main():
 	IJ.run("Close All")
 	closeAll()
-	cellpose_roi_dir, = generate_required_directories(raw_dir.getParent(), "Cellpose")
-	# base_path = os.path.dirname(str(import_dir))
-	
 	# Run the batch_open_images() function using the Scripting Parameters.
 	image_paths = batch_open_images(raw_dir,
 								split_string(file_types),
 								split_string(filters),
 								do_recursive
 								)
-	IJ.log("Cellpose Directory: {}".format(cellpose_roi_dir))
+	
 	for image_path in image_paths:
 		IJ.run("Close All")
 		closeAll()
-		save_rois=True
-		image_string = "raw_path='{}' segchan='{}' cellposediam='{}' model='{}'".format(image_path, segChan, cellposeDiam, model)
+		save_rois = "True" # Required for boolean parameter passing "True", rather than as python's True as "1".
+		image_string = "raw_path='{}' segchan='{}' cellposediam='{}' model='{}', save_rois='{}'".format(image_path, segChan, cellposeDiam, model, save_rois)
 		IJ.log(image_string)
 		IJ.run("Cellpose Image", image_string)
 		label_title = IJ.getImage().title
-		IJ.log("### Saving ROIs ###")
-		cellpose_roi_path = os.path.join(cellpose_roi_dir,str(label_title)+"_RoiSet.zip")
-		RM_fiber = RoiManager()
-		rm_fiber = RM_fiber.getRoiManager()
-		rm_fiber.save(cellpose_roi_path)
 	IJ.log("Done!")
-	# TODO: Metadata
 
 if __name__ in ['__builtin__','__main__']:
 	main()
--- src/main/resources/scripts/FiberSight_Beta/Single_Image_Tools/Cellpose_Image.py
+++ src/main/resources/scripts/FiberSight_Beta/Single_Image_Tools/Cellpose_Image.py
@@ -1,7 +1,8 @@
 #@ File(label='Select a raw image', description="<html>Image should ideally be in TIF format</html>", style='file') raw_path
-#@ Integer (label="Border Channel", description="<html>Unused if the image is single-channel or brightfield</html>", min=0, max=10, value=0) segChan
-#@ Integer (label="Object Diameter",  description="<html>Set 0 to use Cellpose auto-detection </html>", min=0, max=200, value=0) cellposeDiam
-#@ String (choices={"cyto3", "cyto2", "PSR", "WGA", "HE"}, description="The type of model to use", value="cyto3", style="radioButtonHorizontal") model
+#@ Integer (label="Segmentation Channel", description="<html>Set 0 to use gray-scale, otherwise channels are indexed from 1</html>", min=0, max=10, value=0) segChan
+#@ Integer (label="Object Diameter",  description="<html>Set 0 to use Cellpose auto-detection (available only on cyto3 model) </html>", min=0, max=200, value=0) cellposeDiam
+#@ String (choices={"cyto3", "PSR_9", "WGA_21", "HE_30"}, description="The type of model to use", value="cyto3", style="radioButtonHorizontal") model
+#@ Boolean (label="Autosave ROIs to standard location?", description="Standard location is in a folder one level above the image folder", value=True) save_rois
 
 """
 Runs Cellpose on a single image, and produces a set of ROIs. 
@@ -15,40 +16,35 @@ from ij.plugin.frame import RoiManager
 from jy_tools import closeAll, list_files, reload_modules
 from image_tools import runCellpose, detectMultiChannel, \
 batch_open_images, split_string, convertLabelsToROIs, read_image
+from utilities import get_model_path, download_model
+from analysis_setup import FileNamer
+reload_modules()
 
 def main():
-	raw_path_string = raw_path.getAbsolutePath()
-	IJ.log("Raw Path: {}".format(raw_path_string))
-	homedir=os.path.expanduser("~")
-	model_dir=os.path.join(homedir,".cellpose","models")
-	if str(model) == "cyto2":
-		model_name = "cyto2_cp3"
-	elif str(model) == "PSR":
-		model_name = os.path.join(model_dir, "PSR_9")
-		IJ.log("Attention: PSR model will not be able to automatically estimate fiber diameter, use Cyto2/3 for diameter estimation")
-	elif str(model) == "WGA":
-		model_name = os.path.join(model_dir, "WGA_21")
-		IJ.log("Attention: WGA model will not be able to automatically estimate fiber diameter, use Cyto2/3 for diameter estimation")
-	elif str(model) == "HE":
-		model_name = os.path.join(model_dir, "HE_30")
-		IJ.log("Attention: HE model will not be able to automatically estimate fiber diameter, use Cyto2/3 for diameter estimation")
-	else:
-		model_name = str(model)
-		
-	if model_name.split("/")[-1] in os.listdir(model_dir):
-		IJ.log("Model {} found".format(model))
-	else:
-		default_model = "cyto3"
-		IJ.log("Model {} NOT found, using default model {}".format(model, default_model))
+	namer = FileNamer(raw_path.path)
+	IJ.log("Raw Path: {}".format(namer.image_path))
+	model_path = get_model_path(model)
 	
-	original_imp = read_image(raw_path_string)
+	if not os.path.exists(model_path):
+		IJ.log("Downloading model to {}".format(model_path))
+		download_model(model)
+
+	original_imp = read_image(namer.image_path)
 	original_imp.show()
 	
 	if detectMultiChannel(original_imp):
-		IJ.log("Multiple channels detected; splitting image")
-		channels = ChannelSplitter.split(original_imp)
-		channels[segChan-1].show() # Selects the channel to segment, offset by 1 for indexing
-		original_imp.hide()
+		if segChan == 0:
+			IJ.log("Processing image in gray-scale")
+			pass # Keep image in gray-scale
+		elif segChan > original_imp.NChannels:
+			IJ.error("Selected segmentation channel ({}) exceeds total number of channels ({}), segmenting on gray-scale instead".format(segChan, original_imp.NChannels))
+			pass
+		else:
+			IJ.log("Multiple channels detected; splitting image")
+			IJ.log("Extracting and segmenting channel {}".format(segChan))
+			channels = ChannelSplitter.split(original_imp)			
+			channels[segChan-1].show() # Selects the channel to segment, offset by 1 for indexing
+			original_imp.hide()
 	
 	image_to_segment = IJ.getImage()
 	
@@ -56,12 +52,9 @@ def main():
 	IJ.log("### Running Cellpose on "+image_to_segment.title+" ###")
 	
 	start = time.time()
-	if model_name == "cyto3" or model_name=="cyto2":
-		runCellpose(image_to_segment, model_type="cyto3", model_path = "", env_type = "conda", diameter=cellposeDiam, cellprob_threshold=0.0, flow_threshold=0.4, ch1=0, ch2=0)
-	else:
-		runCellpose(image_to_segment, model_type="", model_path = model_name, env_type = "conda", diameter=cellposeDiam, cellprob_threshold=0.0, flow_threshold=0.4, ch1=0, ch2=0)
+	
+	runCellpose(image_to_segment, model_type="", model_path = model_path, env_type = "conda", diameter=cellposeDiam, cellprob_threshold=0.0, flow_threshold=0.4, ch1=0, ch2=0)
 
-	# runCellpose(image_to_segment, cellposeModel=model_name, cellposeDiameter=cellposeDiam)
 	finish = time.time()
 	time_in_seconds = finish-start
 	IJ.log("Time to run Cellpose = {:.2f} seconds".format(time_in_seconds))
@@ -72,6 +65,11 @@ def main():
 	rm_fiber = convertLabelsToROIs(imp_labels) # Saves the ROIs at the end
 	num_detections = rm_fiber.getCount()
 	IJ.log("Number of Detected Fibers: {}".format(num_detections))
+	if save_rois:
+		IJ.log("### Saving ROIs ###")
+		IJ.log("Saving to standard location: {}".format(namer.fiber_roi_path))
+		namer.generate_directory("fiber_rois")
+		rm_fiber.save(namer.fiber_roi_path)
 	
 if __name__ == "__main__":
 	IJ.run("Close All")
