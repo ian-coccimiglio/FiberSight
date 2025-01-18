2fdfe04178d181be07622d08cea3414bcbad5a0f
ian-coccimiglio <icoccimi@gmail.com>
2025-01-18 11:30:08 -0800

    Moved script functions to Analysis class methods

--- src/main/resources/analysis_setup.py
+++ src/main/resources/analysis_setup.py
@@ -50,12 +50,83 @@ class AnalysisSetup:
 		self.imp_channels = self.split_channels() # Splits the channels into respective order
 		self.imp_scale = self.imp.getCalibration().pixelWidth # microns per pixel
 		self.channel_dict = self.remap_channels()
+		self.composite_string = self.get_channel_colormap()
 		self.check_channels()
 
 		self.ft_sigma_blur=ft_sigma_blur
 		self.border_channel, self.dapi_channel, self.ft_channels = self.rename_channels() # Renames channels according
 		self.Morph, self.CN, self.FT = self.assign_analyses()
 		self.drawn_border_roi = self.get_manual_border()
+	
+	def save_results(self):
+		"""
+		Creates a directory in standard location, then saves the results to it.
+		"""
+		self.namer.create_directory("results")
+		IJ.saveAs("Results", self.namer.results_path) if IJ.isResultsWindow() else IJ.log("Results window wasn't opened!")
+	
+	def create_figures(self):
+		self.namer.create_directory("figures")
+		if self.Morph:
+			morphology_image = self.imp.duplicate()
+			for label in range(self.rm_fiber.getCount()):
+				self.rm_fiber.rename(label, str(label+1))
+			
+			Prefs.useNamesAsLabels = True;
+			self.rm_fiber.runCommand(morphology_image, "Show All with Labels")
+			IJ.run(morphology_image, "Labels...",  "color=red font="+str(24)+" show use bold")
+			self.rm_fiber.moveRoisToOverlay(morphology_image)
+			morphology_image.hide()
+			flat_morphology_image = morphology_image.flatten()
+			morphology_path = os.path.join(self.namer.figures_dir, "{}_morphology".format(self.namer.base_name))
+			IJ.saveAs(flat_morphology_image, "Jpg", morphology_path)
+			pass # Morphology image
+	
+		if self.CN:
+			composite_string = " ".join(['c3=[DAPI]', 'c6=[Fiber Border]'])
+			self.border_channel.show()
+			self.dapi_channel.show()
+			IJ.run("Merge Channels...", composite_string+" create keep")
+			CN_image = IJ.getImage()
+			self.border_channel.hide()
+			self.dapi_channel.hide()
+	#		flat_CN_image = CN_image.flatten()
+	#		CN_path = os.path.join(self.namer.figures_dir, "{}_central_nucleation".format(self.namer.base_name))
+			# IJ.saveAs(flat_CN_image, "Jpg", CN_path)
+			
+	#		flat_gradient_nucleation_image = gradient_nucleation_image.flatten()
+	#		gradient_nucleation_path = os.path.join(self.namer.figures_dir, "{}_gradient_nucleation".format(self.namer.base_name))
+			#IJ.saveAs(flat_gradient_nucleation_image, "Jpg", gradient_nucleation_path)
+			pass # Central-nucleation composite image
+			# Multiple erosion image with fraction as image label
+			
+		if self.FT:
+			pass # Fiber-Type composite image
+	
+	def isBrightfield(self):
+		"""
+		Checks if an image is Brightfield based on if the first channel is labelled as 'Fiber Borders',
+		and the subsequent channels are labelled as 'None'.
+		"""
+		return self.all_channels[0] == "Fiber Border" and all(ch is None for ch in self.all_channels[1:4])
+	
+	def get_fiber_border_channel_position(self):
+		"""
+		Returns the index of the fiber border channel selected, offset by positive 1
+		"""
+		for enum, channel in enumerate(self.all_channels):
+			if channel == "Fiber Border":
+				return enum+1
+	
+	def get_channel_colormap(self):
+		composite_list = []
+		cmap = {"Type IIx":"c1", "Type IIa":"c5", "DAPI":"c3", "Fiber Border":"c6", "Type I":"c7"}
+		for channel in self.all_channels:
+			if channel is not None:
+				color = cmap[channel]
+				composite_list.append("{}=[{}]".format(color, channel))
+		composite_string = " ".join(composite_list)
+		return composite_string
 	
 	def get_fiber_rois(self, fiber_roi_path=None):
 		fiber_roi_path = self.namer.fiber_roi_path if fiber_roi_path is None else fiber_roi_path
